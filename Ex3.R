75f0c096a7c7812867ab074fa941a4b7d61e55be
sil-pixel <silpa2408@gmail.com>
2025-09-26 14:15:03 +0200

    Update Ex3.R

--- Ex3.R
+++ Ex3.R
@@ -1,4 +1,14 @@
 library(mdmb)
+library(mice)
+
+
+nhanes2$hyp <- as.numeric(nhanes2$hyp=="yes")
+nhanes2 <- nhanes2[, c("age", "hyp", "bmi", "chl")]
+
+md.pattern(nhanes2)
+
+complete_fit <- lm(chl ~ age + hyp + bmi, data=nhanes2)
+summary(complete_fit)
 
 # cholestrol is a dependent variable on age, hypertension and bmi
 dep <- list(model="linreg", formula=chl~age+hyp+bmi)
@@ -9,3 +19,38 @@ ind <- list(ind_hyp, ind_bmi)
 
 fit <- frm_em(dat=nhanes2, dep=dep, ind=ind, verbose=FALSE)
 summary(fit)
+
+nhanes2Big <- nhanes2[rep(1:nrow(nhanes2), 100), ]
+start_time <- proc.time()
+
+fit <- lm(chl ~ age + hyp + bmi, data=nhanes2Big)
+proc.time() - start_time
+
+start_time <- proc.time()
+fit <- frm_em(dat=nhanes2Big, dep=dep, ind=ind, verbose=FALSE)
+proc.time() - start_time
+
+##############################################################
+
+nhanes2.complete <- mice(data=nhanes2, printFlag = FALSE, seed = 1)
+mice_fit <- with(data=nhanes2.complete, expr = lm(formula = chl ~ age + hyp + bmi))
+pooled_mice_fit <- pool(object=mice_fit)
+summary(pooled_mice_fit)
+
+
+nhanes2.complete <- mice(data=nhanes2Big, printFlag = FALSE, seed = 1)
+mice_fit <- with(data=nhanes2.complete, expr = lm(formula = chl ~ age + hyp + bmi))
+pooled_mice_fit <- pool(object=mice_fit)
+summary(pooled_mice_fit)
+
+
+
+nhanes2.complete <- mice(data=nhanes2, printFlag = FALSE, seed = 1, m = 100, maxit = 100)
+mice_fit <- with(data=nhanes2.complete, expr = lm(formula = chl ~ age + hyp + bmi))
+pooled_mice_fit <- pool(object=mice_fit)
+summary(pooled_mice_fit)
+
+nhanes2.complete <- mice(data=nhanes2Big, printFlag = FALSE, seed = 1, m = 10, maxit = 10)
+mice_fit <- with(data=nhanes2.complete, expr = lm(formula = chl ~ age + hyp + bmi))
+pooled_mice_fit <- pool(object=mice_fit)
+summary(pooled_mice_fit)
